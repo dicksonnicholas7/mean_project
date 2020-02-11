1645b8861ea42e02723805466a7ce88400b32f59
dicksonnicholas7 <dicksonnicholas7@gmail.com>
2020-02-11 09:03:00 +0000

    changes to files

--- server.js
+++ server.js
@@ -4,4 +4,14 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.post("/api/blogpost", createPost);
+
+function createPost(req,res){
+
+    console.log("hello from server");
+}
+
+
 app.listen(7000);
+
+console.log("started listening to port ");
