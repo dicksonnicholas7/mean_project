7ab22c66be0a8ee2024251137291e5595cd02256
dicksonnicholas7 <dicksonnicholas7@gmail.com>
2020-02-11 13:39:15 +0000

    Updates

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require ('express');
+var bodyParser = require('body-parser')
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/api/blogpost", createPost);
 
 function createPost(req,res){
-
-    console.log("hello from server");
+    var post = req.body;
+    console.log(post);  
 }
 
 
